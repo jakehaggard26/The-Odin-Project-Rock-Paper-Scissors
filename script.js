9070dad4cc715c53a06a5f615fe972ebe6aad993
jakehaggard26 <jhaggard26@gmail.com>
2026-05-24 07:37:20 -0400

    Add Flex to Game Container

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-humanScore = 0;
-computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
+let roundNum = 1;
 
 function createScoreBoardText() {
+    const round = document.querySelector("#round");
+    round.textContent = `Round ${roundNum}`
     const score = document.querySelector("#score");
-    console.log(score);
     score.textContent = `Your Score: ${humanScore} vs Computer Score ${computerScore}`;
 }
 
