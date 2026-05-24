195a9c565aaa82b070ac98ea411e6e606080e541
jakehaggard26 <jhaggard26@gmail.com>
2026-05-24 08:09:38 -0400

    Add logic for player choosing rock

--- script.js
+++ script.js
@@ -1,18 +1,124 @@
-let humanScore = 0;
+let playerScore = 0;
 let computerScore = 0;
 let roundNum = 1;
 
-function createScoreBoardText() {
+const rockBtn = document.querySelector("#rock");
+rockBtn.addEventListener("click", () => updateGame("rock"));
+
+const paperBtn = document.querySelector("#paper");
+paperBtn.addEventListener("click", () => updateGame("paper"));
+
+const scissorsBtn = document.querySelector("#scissors");
+scissorsBtn.addEventListener("click", () => updateGame("scissors"));
+
+function updateScoreBoardText() {
     const round = document.querySelector("#round");
     round.textContent = `Round ${roundNum}`
     const score = document.querySelector("#score");
-    score.textContent = `Your Score: ${humanScore} vs Computer Score ${computerScore}`;
+    score.textContent = `Your Score: ${playerScore} vs Computer Score ${computerScore}`;
 }
 
+function resetGame()
+{
+    humanScore = 0;
+    computerScore = 0;
+    roundNum = 1;
+}
+
+function updateGame(playerInput) {
+
+    // Generate Computer Pick
+    let computerInput = generateComputerPick(); 
+
+    // Check Input to Determine Winner 
+    console.log(`Player: ${playerInput}`);
+    console.log(`Computer: ${computerInput}`);
+
+    // If player picks rock
+    if(playerInput == "rock")
+    {
+        // if computer picks rock (Draw)
+        if(computerInput == "rock") 
+        {
+            console.log("Draw. Both players picked Rock");
+        }
+        
+        // if computer picks paper (Loss)
+        else if(computerInput == "paper")
+        {
+            console.log("Player Loss. Paper (computer) beats Rock (player)");
+            computerScore++;
+        }
+
+        // if computer picks scissors (Win)
+        else 
+        {
+            console.log("Player Win. Rock (computer) beats Scissors (player)");
+            playerScore++;
+        }
+            
+    }
+
+    // If player picks paper
+    else if(playerInput == 'paper')
+    {
+        // if computer picks rock (Win)
+
+        // if computer picks paper (Draw)
+
+        // if computer picks scissors (Loss)
+
+    }
+    
+    // If player picks scissors
+    else
+    {
+        // if computer picks rock (Loss)
+
+        // if computer picks paper (Win)
+
+        // if computer picks scissors (Draw)
+        
+    }
+
+    // Increment Round Number
+    roundNum++;
+    
+    // Update Scoreboard
+    updateScoreBoardText();
+
+    // Round Check (If max rounds reached, reset the game)
+    // if(roundNum > 5)
+    //{
+        //resetGame();
+    //}
+// }
+
+function generateComputerPick()
+{
+    let num = Math.floor(Math.random() * 3)
+    let pick = "";
+    switch(num)
+    {
+        case 0: 
+            pick = "rock";
+            break;
+        case 1: 
+            pick = "paper";
+            break;
+        case 2: 
+            pick = "scissors";
+            break;
+        default: 
+            pick = "rock";
+    }
+
+    return pick;
+}
 
 function startGame()
 {
-    createScoreBoardText();
+    updateScoreBoardText();
 }
 
 startGame();
