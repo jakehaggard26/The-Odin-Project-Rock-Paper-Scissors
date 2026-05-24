fc185ccf0d22d693b1d941096053b09efa2de9b0
jakehaggard26 <jhaggard26@gmail.com>
2026-05-24 07:23:07 -0400

    Add Score and Play Buttons

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-ECHO is on.
+humanScore = 0;
+computerScore = 0;
+
+function createScoreBoardText() {
+    const score = document.querySelector("#score");
+    console.log(score);
+    score.textContent = `Your Score: ${humanScore} vs Computer Score ${computerScore}`;
+}
+
+
+function startGame()
+{
+    createScoreBoardText();
+}
+
+startGame();
